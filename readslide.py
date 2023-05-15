bdd5a128cb0beda43355cf226ae4756b4f9dd76f
mark <shundyning@163.com>
2023-05-15 12:21:12 +0800

    add test.py

--- readslide.py
+++ readslide.py
@@ -5,4 +5,16 @@ if hasattr(os, 'add_dll_directory'):
         import openslide
 else:
     import openslide
-p = openslide.OpenSlide(r'D:\slidedata\1-50\02585_1_SCC_1.ndpi')
+import shutil
+class SlideReader:
+    def __init__(self, file_path):
+        self.slidefile = file_path
+        self.file_dir, self.filename = os.path.split(file_path)
+        self.tempslide = os.path.join(self.file_dir, 'temp.ndpi')
+        os.rename(file_path, self.tempslide)
+        try:
+            self.slide = openslide.OpenSlide(self.tempslide)
+        except:
+            os.rename(self.tempslide, file_path)
+        os.rename(self.tempslide, file_path)
+# p = openslide.OpenSlide(r'D:\slidedata\1-50\02585_1_SCC_1.ndpi')
